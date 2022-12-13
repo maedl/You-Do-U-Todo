6431f32e5be4947236de9c39e4169b48686d1e87
Max Edlund <maxedlund88@gmail.com>
2022-12-13 01:54:03 +0100

    add test func for todo-container, btn listeners

--- src/main.js
+++ src/main.js
@@ -1,6 +1,58 @@
+import { doc } from 'prettier';
 import './style/style.scss';
 
-let headingEl = document.createElement('h1');
-headingEl.innerHTML = 'TODO: Create Todo App';
+// Variables
 
-document.querySelector('main').append(headingEl);
+// Elements
+
+const todoContainer = document.querySelector('.todo-container');
+const addTodoBtn = document.querySelector('.add-todo');
+const sortingBtn = document.querySelector('.sorting-icon');
+const helpBtn = document.querySelector('.help-icon');
+
+// Functions
+
+function setEventListeners() {
+  addTodoBtn.addEventListener('click', openAddTodo);
+  sortingBtn.addEventListener('click', openSortingMenu);
+  helpBtn.addEventListener('click', openHelp);
+}
+
+/**
+ * testar layout med 8 exempel-todos
+ */
+function openAddTodo() {
+  todoContainer.innerHTML = `
+  <h2>
+    To do:
+  </h2>
+  `;
+
+  for (let i = 0; i < 8; i++) {
+    todoContainer.innerHTML += `
+    <div class="todo">
+    <div class="left-grid">
+      <input type="checkbox">
+    </div>
+    <div class="middle-grid">
+      <p>Gör en todo-app</p>
+      <div class="date-section">Datum</div>
+    </div>
+    <div class="right-grid">
+      <div class="icon1">1</div>
+      <div class="icon2">2</div>
+    </div>
+    `;
+  }
+}
+
+function openSortingMenu() {
+  alert('sortera här');
+}
+
+function openHelp() {
+  alert('I need help asap');
+}
+
+// Program Flow
+setEventListeners();
