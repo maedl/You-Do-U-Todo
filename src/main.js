e952bd1b98ac184a56daad8a2cc491b8dcd17bff
Max Edlund <maxedlund88@gmail.com>
2022-12-14 03:23:25 +0100

    add functionality for plus-button

--- src/main.js
+++ src/main.js
@@ -5,31 +5,38 @@ import './style/style.scss';
 
 // Elements
 
-const todoContainer = document.querySelector('.todo-container');
-const addTodoBtn = document.querySelector('.add-todo');
+const todoSection = document.querySelector('.todo-section');
+const inputContainer = document.querySelector('.input-section');
+const closeInputBtn = document.querySelector('.close-input');
+const addTodoBtn = document.querySelector('.open-add-todo-btn');
 const sortingBtn = document.querySelector('.sorting-icon');
 const helpBtn = document.querySelector('.help-icon');
 
 // Functions
 
 function setEventListeners() {
-  addTodoBtn.addEventListener('click', openAddTodo);
+  addTodoBtn.addEventListener('click', toggleAddTodo);
+  closeInputBtn.addEventListener('click', toggleAddTodo)
   sortingBtn.addEventListener('click', openSortingMenu);
   helpBtn.addEventListener('click', openHelp);
+}
+
+function toggleAddTodo() {
+  inputContainer.classList.toggle('input-active');
 }
 
 /**
  * testar layout med 8 exempel-todos
  */
-function openAddTodo() {
-  todoContainer.innerHTML = `
+function addExampleTodos() {
+  todoSection.innerHTML = `
   <h2>
     To do:
   </h2>
   `;
 
   for (let i = 0; i < 8; i++) {
-    todoContainer.innerHTML += `
+    todoSection.innerHTML += `
     <div class="todo">
     <div class="left-grid">
       <input type="checkbox">
@@ -56,3 +63,4 @@ function openHelp() {
 
 // Program Flow
 setEventListeners();
+addExampleTodos();
