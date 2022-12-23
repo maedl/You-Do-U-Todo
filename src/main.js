f4609701a3b21678ea8753840d384d16c9a0bd79
Max Edlund <maxedlund88@gmail.com>
2022-12-23 02:57:24 +0100

    fix buggy behaviour of delete all button

--- src/main.js
+++ src/main.js
@@ -256,6 +256,7 @@ function deleteAllCompleted() {
   }
   setArrToStorage();
   renderTodos();
+  delAllCompleteBtn.classList.toggle('hidden');   // if button is clicked, it does its job and disappears
 }
 
 function handleCheckboxes(e) {
@@ -283,21 +284,26 @@ function manageCompleteStatus(e) {
   renderTodos();
 }
 
-// TODO: more fun stats?
+/**
+ * 
+ * @param {number} doneCounter 
+ */
 function renderInfoBar(doneCounter) {
   let done = doneCounter;
   todoAmountSpan.innerText = '';
   if (todoArr.length > 0) {
     todoAmountSpan.innerText = `${done} / ${todoArr.length} completed`;
+
+    if (todoArr.length === doneCounter) {
+      delAllCompleteBtn.classList.remove('hidden');
+    }
   }
-  if (todoArr.length === doneCounter) {
-    toggleDeleteAllBtn();
+
+  if (todoArr.length > 0 && doneCounter === 0) {
+    delAllCompleteBtn.classList.add('hidden');
   }
 }
 
-function toggleDeleteAllBtn() {
-  delAllCompleteBtn.classList.toggle('hidden');
-}
 function clearForm() {
   document.querySelector('.input-section').reset();
 }
