cd21d3288d9a2764e22be854bbd6e3a76717b362
Max Edlund <maxedlund88@gmail.com>
2023-02-10 18:58:42 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 
-/*
-Om ditt repo har adressen https://github.com/Medieinstitutet/fed22d-js-grundkurs-jenni-wumpus,
-då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
- */
-
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-todo-maedl/',
+  base: '/You-Do-U-Todo',
   define: {
     'process.env': {},
   },
